0dbc329f20c9dccadb29094be1cb81f246c62cfd
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2017-12-21 12:58:49 +0900

    org

--- MkLatex.rb
+++ MkLatex.rb
@@ -1,36 +1,32 @@
 require 'kconv'
 # make content.tex from directories.
 
-content=''
+content = ''
 
-ARGV.each{|argv|
-  dir_name=File::basename(argv)
-  hiki_inLatex=dir_name+"/"+dir_name
-  puts hiki_file=hiki_inLatex+".tex"
-  text=NKF.nkf("-w",File.read(hiki_file))
-  ifDocument=false
-  text.each_line{|line|
+ARGV.each do |argv|
+  dir_name = File.basename(argv)
+  hiki_inLatex = dir_name + '/' + dir_name
+  puts hiki_file = hiki_inLatex + '.tex'
+  text = NKF.nkf('-w', File.read(hiki_file))
+  ifDocument = false
+  text.each_line do |line|
     case line
     when /\\begin\{document\}/
-      ifDocument=true
+      ifDocument = true
     when /\\end\{document\}/
-      ifDocument=false
+      ifDocument = false
     when /^\\input\{(.*)\}/
-      content << "\\input\{"+dir_name+"/#{$1}\} \n"
+      content << "\\input\{" + dir_name + "/#{Regexp.last_match(1)}\} \n"
     else
-      if ifDocument then
-        content << line
-      end
+      content << line if ifDocument
     end
-  }
-  tmp=dir_name+"/figures"+"/*.eps"
-  Dir::glob(tmp).each {|file|
+  end
+  tmp = dir_name + '/figures' + '/*.eps'
+  Dir.glob(tmp).each do |file|
     p file
-#    system("cp #{file} ./figures")
-  }
-}
-file1=File.open("content.tex","w")
-file1.print NKF.nkf("-s",content)
+    #    system("cp #{file} ./figures")
+  end
+end
+file1 = File.open('content.tex', 'w')
+file1.print NKF.nkf('-s', content)
 file1.close
-
-
--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-# MapleText
+* +STARTUP: indent nolineimages nofold
+  :PROPERTIES:
+  :CUSTOM_ID: startup-indent-nolineimages-nofold
+  :END:
+
+-  MapleText
 
 Mapleの自習用テキストのソースです．latexで作られるようにしています．
-hikiでの提供も[Mapleテキスト](http://ist.ksc.kwansei.ac.jp/~nishitani/MapleHiki/)からしています．
+hikiでの提供も[[http://ist.ksc.kwansei.ac.jp/~nishitani/MapleHiki/][Mapleテキスト]]からしています．
 
-# License
-[CC BY-NC](https://creativecommons.org/licenses/by-nc/4.0/) by Shigeto R. Nishitani
+-  MaplePrime/
+-  MapleProgramming/
+-  NumMaple/
+
+-  compile
+-  latexはそのまま
+-  hikiへは，それぞれのdirectoryでrake
+   hikiとするとすべて自動でやってくれる．
+
+* BEGIN\_QUOTE
+  :PROPERTIES:
+  :CUSTOM_ID: begin_quote
+  :END:
+
+For making maple hiki -v, --version show program Version. --init [NAME]
+initialize NAME directory. --figures [NAME] gather and convert figures
+in NAME dir. --scale [VAL]% set scale for convert figures. --level [VAL]
+set level for head start level . --hiki [NAME] make hiki contents from
+NAME directory. #END\_QUOTE
+
+-  License [[https://creativecommons.org/licenses/by-nc/4.0/][CC BY-NC]]
+   by Shigeto R. Nishitani
